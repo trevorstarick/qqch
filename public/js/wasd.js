2804b35637fd2ee1724fa39b767a6dbe9bba55d8
Trevor Starick <trevor.starick@gmail.com>
2014-07-19 23:41:06 -0400

    Created WASD.js to replace keypressjs

---
+++ public/js/wasd.js
@@ -0,0 +1,9 @@
+/*
+  WASD.js version 1.0.0 (c) 2014 Trevor Starick.
+  Licensed under the Apache License, Version 2.0
+  http://www.apache.org/licenses/LICENSE-2.0
+*/
+
+(function() {
+
+}).call(this);
