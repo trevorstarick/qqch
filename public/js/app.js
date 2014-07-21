bc385caba410bbe32ead0d86476ca21d8f1509dd
Trevor Starick <trevor.starick@gmail.com>
2014-07-20 22:50:11 -0400

    2d object mapping.

--- public/js/app.js
+++ public/js/app.js
@@ -238,9 +238,16 @@ window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequest
     }
   };
 
-  var Map = function() {};
+  var Map = function() {
+    this.array = [];
+  };
   Map.prototype = {
-    init: function() {},
+    init: function() {
+      this.array = new Array(width / 8);
+      for (i = 0; i < width / 8; i++) {
+        this.array[i] = new Array(height / 8);
+      }
+    },
     update: function() {},
     render: function() {},
     slide: function(direction) {},
